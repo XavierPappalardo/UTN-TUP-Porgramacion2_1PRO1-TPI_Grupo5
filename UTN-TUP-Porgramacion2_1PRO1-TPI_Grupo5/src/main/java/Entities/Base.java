c369bc79a149928ee279e34859bfbf820d6d1c19
LauGomezCanovas <lautarolegogomez@gmail.com>
2026-06-19 18:05:33 -0300

    Añado logica ProductoService / Update Base

--- UTN-TUP-Porgramacion2_1PRO1-TPI_Grupo5/src/main/java/Entities/Base.java
+++ UTN-TUP-Porgramacion2_1PRO1-TPI_Grupo5/src/main/java/Entities/Base.java
@@ -10,9 +10,37 @@ public abstract class Base {
 
     protected LocalDateTime createdAt;
 
+    // Constructor
+
     public Base(Long id, boolean eliminado, LocalDateTime createdAt) {
         this.id = id;
         this.eliminado = eliminado;
+        this.createdAt = createdAt;
+    }
+
+    // Getters y Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
 }
--- UTN-TUP-Porgramacion2_1PRO1-TPI_Grupo5/src/main/java/Services/ProductoService.java
+++ UTN-TUP-Porgramacion2_1PRO1-TPI_Grupo5/src/main/java/Services/ProductoService.java
@@ -1,4 +1,81 @@
 package Services;
+import java.util.List;
+import java.util.ArrayList;
+
+import Entities.Categoria;
+import Entities.Producto;
+import Exceptions.StockInvalidoException;
 
 public class ProductoService {
+
+    private List<Producto> productos;
+
+    public ProductoService(){
+        productos = new ArrayList<>();
+    }
+
+    // Metodos
+
+    // Crear Producto
+
+    public void crearProducto(Producto producto) {
+        if (producto.getPrecio() < 0) {
+            throw new IllegalArgumentException(
+                    "El precio no puede ser negativo."
+            );
+        }
+        if (producto.getStock() < 0) {
+            throw new StockInvalidoException(producto.getNombre(), );
+        }
+
+        productos.add(producto);
+    }
+
+    // Listar Producto
+
+    public void listarProductos() {
+        if (productos.isEmpty()) {
+            System.out.println("No hay productos cargados.");
+            return;
+        }
+
+        for (Producto producto : productos) {
+            if (!producto.isEliminado()) {
+                System.out.println(producto);
+            }
+        }
+    }
+
+    // Buscar por ID
+
+    public Producto buscarPorId(Long id) {
+        for (Producto producto : productos) {
+            if (producto.getId().equals(id) && !producto.isEliminado()) {
+                return producto;
+            }
+        }
+
+        return null;
+    }
+
+    // Editar Producto
+
+    public boolean editarProducto(Long id,
+                                  String nombre,
+                                  Double precio,
+                                  int stock,
+                                  Categoria categoria) {
+        Producto producto = buscarPorId(id);
+
+        if(producto == null) {
+            return false;
+        }
+
+        producto.setNombre(nombre);
+        producto.setPrecio(precio);
+        producto.setStock(stock);
+        producto.setCategoria(categoria);
+
+        return true;
+    }
 }
