d20d5399b80aea090ad12f8eb3138de124c0be8e
Lauticodemaster <lautigamermendoza@gmail.com>
2026-06-19 14:29:56 -0300

    Update MailDuplicadoException.java

--- UTN-TUP-Porgramacion2_1PRO1-TPI_Grupo5/src/main/java/Exceptions/MailDuplicadoException.java
+++ UTN-TUP-Porgramacion2_1PRO1-TPI_Grupo5/src/main/java/Exceptions/MailDuplicadoException.java
@@ -1,4 +1,26 @@
 package Exceptions;
 
-public class MailDuplicadoException {
+/**
+ * Se lanza cuando se intenta crear o editar un Usuario con un mail
+ * que ya existe en la colección (el mail debe ser único según las reglas
+ * de negocio definidas en la consigna).
+ *
+ * Ejemplo de uso:
+ *   throw new MailDuplicadoException("juan@gmail.com");
+ */
+public class MailDuplicadoException extends Exception {
+
+    private final String mail;
+
+    /**
+     * @param mail El correo electrónico que ya estaba registrado.
+     */
+    public MailDuplicadoException(String mail) {
+        super("El mail '" + mail + "' ya está registrado. Ingrese uno diferente.");
+        this.mail = mail;
+    }
+
+    public String getMail() {
+        return mail;
+    }
 }
